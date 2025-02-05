64ab1fa64284ee88b90c55b9dba82d999d47d165
AbdullahAfzal21 <abdullah.54aug24webgpt@gmail.com>
2025-02-05 22:20:30 +0500

    emp

--- src/components/experience.jsx
+++ src/components/experience.jsx
@@ -1,20 +1,85 @@
-import { Typography } from "@mui/material"
-
+import { Container, Grid, Typography, Box } from "@mui/material";
+import Seebiz from "../../img/seebiz.png";
 
 function Experience() {
   return (
-<div>
-<Typography
-      variant="h4"
+    <Box
       sx={{
-        textShadow: "3px 3px 5px rgba(0,0,0,0.4)", 
-        textAlign:"center",my:"70px",fontSize:"40px"
+        backgroundColor: "white",
+        color: "black",
+        minHeight: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        px: 3, 
+        pt: {xs: 8, md: 0},
+        pb: {xs: 3, md: 0}
       }}
     >
-      Experience
-    </Typography>
-</div>
-  )
+      <Typography
+        variant="h4"
+        sx={{
+          textShadow: "3px 3px 5px rgba(0,0,0,0.4)",
+          textAlign: "center",
+          fontSize: { xs: "36px", md: "40px" }, 
+          mb: 10,
+        }}
+      >
+        Experience
+      </Typography>
+
+      <Container maxWidth="lg">
+        <Grid container spacing={4} alignItems="center">
+          <Grid
+            item
+            xs={12}
+            md={4}
+            display="flex"
+            justifyContent="center"
+            sx={{ mb: { xs: 3, md: 0 } }}
+          >
+            <Box
+              component="img"
+              src={Seebiz}
+              alt="Seebiz logo"
+              sx={{
+                width: { xs: "180px", md: "250px" }, 
+                height: { xs: "180px", md: "250px" },
+                borderRadius: "50%",
+                boxShadow: 3,
+              }}
+            />
+          </Grid>
+
+          <Grid item xs={12} md={8}>
+            <Box display={{ xs: "flex",justifyContent: "space-between" }}>
+              <Box>
+            <Typography variant="h5" fontWeight="bold">
+              Seebiz Pvt Limited
+            </Typography>
+            <Typography variant="h6" sx={{ my: "5px" }}>
+              MERN Stack Trainee
+            </Typography>
+            </Box>
+            <Box>
+            <Typography variant="body1"  sx={{fontSize:{xs:"0.9rem" , md: "1rem"},pt: 1}}>
+            Aug 2024 – Feb 2025
+      <br />
+
+              Lahore, Pakistan
+            </Typography>
+            </Box>
+
+            </Box>
+            <Typography variant="body1" color="black" sx={{ mb: 3 }}>
+              I completed <strong>MERN Stack (MongoDB, Express.js, React.js, Node.js)</strong> training at Seebiz Pvt. Limited, where I gained extensive experience in full-stack web development. The training lasted <strong>6 months</strong>, divided into structured phases. The first <strong>1.5 months</strong> focused on <strong>HTML, CSS, and JavaScript</strong>, covering <strong>DOM manipulation</strong> and responsive design using <strong>Bootstrap</strong> and <strong>MUI</strong>. The next <strong>1 month</strong> was dedicated to <strong>Advanced JavaScript</strong>, exploring ES6+ features, asynchronous programming, and APIs. The final <strong>3.5 months</strong> were centered on <strong>MERN Stack</strong>, where I built multiple applications, worked on <strong>RESTful APIs</strong>, and developed a <strong>live project</strong>. This hands-on experience enhanced my skills in backend and frontend integration, database management, and modern web development practices.
+            </Typography>
+          </Grid>
+        </Grid>
+      </Container>
+    </Box>
+  );
 }
 
-export default Experience
+export default Experience;
--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -1,10 +1,135 @@
+import { Box, Typography, Grid, Card, Button, CardActions, CardContent, CardMedia } from "@mui/material";
 
-function Projects() {
+import Uni from "../../img/pu.png"
+import Clg from "../../img/punjab college logo.svg"
+import School from "../../img/mgs.png"
+import Projectbg from "../../img/web-project.jpg";
+
+
+
+function Project() {
   return (
-  <h1>
-    Projects
-  </h1>
-  )
+    <div>
+      <Box sx={{ display: "flex", flexDirection: "column", alignItems: "center", width: "100%" }}>
+        <Box
+          sx={{
+            width: "100%",
+           maxHeight: "900px",
+            position: "relative",
+            backgroundImage: `url(${Projectbg})`,
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "cover",
+            paddingBottom:"80px",
+            display: "flex",
+            justifyContent: "center",
+
+            "::before": {
+              content: '""',
+              position: "absolute",
+              top: 0,
+              left: 0,
+              width: "100%",
+              height: "100%",
+              backgroundColor: "black",
+              opacity: 0.7,
+            },
+          }}
+        >
+          <Box
+            sx={{
+              position: "relative",
+              color: "white",
+              zIndex: 1,
+              textAlign: "center",
+              width: { xs: "90%", sm: "100%", md: "75%" },
+              borderRadius: "10px",
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <Typography
+              variant="h4"
+              sx={{
+                textShadow: "3px 3px 5px rgba(0,0,0,0.4)",
+                textAlign: "center",
+                fontSize: "42px",
+                py: "70px",
+              }}
+            >
+  Project
+            </Typography>
+
+            <Grid container spacing={10} sx={{display: "flex", justifyContent: "center", alignItems: "center" }}>
+        <Grid item xs={12} sm={3}>
+        <Card
+  color="neutral"
+  orientation="vertical"
+  size="lg"
+/>
+        </Grid >
+        <Grid item xs={12} sm={3}>
+        <Card sx={{ maxWidth: "100%"}}>
+        <CardMedia
+        component="img"
+        alt="green iguana"
+        height="140"
+        image={Clg }
+        sx={{objectFit: "contain",marginTop:"10px"}}
+      />
+      <CardContent>
+      <Typography gutterBottom variant="h6" component="div">
+        Punjab Group of Colleges 
+        </Typography>
+        <Typography gutterBottom variant="h7" component="div">
+    ICS,Stats
+  </Typography>
+        <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+          Lizards are a widespread group of squamate reptiles, with over 6,000
+          species, ranging across all continents except Antarctica
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Share</Button>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
+        </Grid>
+        <Grid item xs={12} sm={3}>
+        <Card sx={{ maxWidth: "100%" }}>
+        <CardMedia
+        component="img"
+        alt="green iguana"
+        height="140"
+        image={School}
+ sx={{objectFit: "contain",marginTop:"10px"}}
+      />
+      <CardContent>
+      <Typography  variant="h6" component="div">
+Mumtaz Grammer School
+  </Typography>
+  <Typography gutterBottom variant="h7" component="div">
+Matric with Computer Science
+  </Typography>
+        <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+          Lizards are a widespread group of squamate reptiles, with over 6,000
+          species, ranging across all continents except Antarctica
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Share</Button>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
+        </Grid>
+      </Grid>
+           
+     
+          </Box>
+        </Box>
+      </Box>
+    </div>
+  );
 }
 
-export default Projects
+export default Project;
